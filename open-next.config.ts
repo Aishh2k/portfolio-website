7a0313a1f40061f758fae9feae225360376c8055
Aiswarya Jayachandran <aiswarya2000may@gmail.com>
2025-12-29 23:13:18 -0500

    Fix OpenNext config: replace invalid object with defineCloudflareConfig({})

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,9 +1,3 @@
-import type { OpenNextConfig } from "@opennextjs/cloudflare";
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-const config: OpenNextConfig = {
-	incrementalCache: {
-		enabled: false,
-	},
-};
-
-export default config;
+export default defineCloudflareConfig({});
