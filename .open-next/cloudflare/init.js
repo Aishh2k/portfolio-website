b7ab7bdc128fed1ef7e94b49e5998477f5392d3c
Aiswarya Jayachandran <aiswarya2000may@gmail.com>
2025-12-29 23:18:53 -0500

    Fix deploy config: remove invalid skip-cache flag and correct worker path

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1767067513310,
+    __BUILD_TIMESTAMP_MS__: 1767068254796,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
